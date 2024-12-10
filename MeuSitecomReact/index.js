ad784b47b5de933b70d4fc0fd629b2d8d928f9be
Yasmim R.M. <160768211+YASMIMRM06@users.noreply.github.com>
2024-12-10 08:45:24 -0300

    commit

--- MeuSitecomReact/index.js
+++ MeuSitecomReact/index.js
@@ -1,14 +1,12 @@
-// Importa React y ReactDOM
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// Importa el archivo principal de la aplicación
+
 import App from './App';
 
-// Establece el punto de entrada en el DOM
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Renderiza la aplicación dentro del DOM
 root.render(
   <React.StrictMode>
     <App />
